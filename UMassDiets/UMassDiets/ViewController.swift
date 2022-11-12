fa518b203449bdda0f34c4e4491d0293900aac59
Vikram Singh <>
2022-11-11 23:28:43 -0500

    Add base project files

---
+++ UMassDiets/UMassDiets/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UMassDiets
+//
+//  Created by Vikram Singh on 11/11/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
